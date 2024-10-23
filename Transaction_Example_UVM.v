229ae281f759a213524b86eecb8e654cc2f0fbeb
naveenrnq <shankar.neo16@gmail.com>
2024-10-24 01:28:45 +0530

    Transaction_Example_UVM

--- Transaction_Example_UVM.v
+++ Transaction_Example_UVM.v
@@ -1,8 +1,13 @@
-class simple_trans extends uvm_transaction;
-
-	rand data_t data;
-	rand addr_t addr;
-	rand enum {WRITE,READ} kind;
-	constraint c1{addr < 16'h2000; }
+class simple_trans extends uvm_transaction; // Any transaction that we need to 
+					    // define needs to be from either OVM or UVM transaction
+	
+	//Information needed for simple transaction					    
+	rand data_t data; // we need data information
+	rand addr_t addr; // We need address information
+	rand enum {WRITE,READ} kind; // Select the operation. i.e. write or read.
+	constraint c1{addr < 16'h2000;} // It is contraint. Simply a verilog construct. 
+					// This constraint is limiting the
+					// addr to be always be less than
+					// 16'h2000
 	// ………..
 endclass
---
+++ Transaction_Level_modelling.v
@@ -0,0 +1,11 @@
+// Transaction Level Modelling
+
+// Transaction Level Modelling is an approach of multiple processes
+// communicating with each other by sending transaction back and forth through
+// channels.
+
+// Channel can be anything take example here as a FIFO. i.e. to get
+// information and pass information.
+
+
+
